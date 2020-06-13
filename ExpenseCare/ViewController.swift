59531bcbba84ea23a0cfb85ac6666a695015ba99
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-06-14 00:29:51 +0530

    Add SignIn button

--- ExpenseCare/ViewController.swift
+++ ExpenseCare/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
