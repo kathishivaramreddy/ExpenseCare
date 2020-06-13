c9ec47ce41d563b50bf32c7fb67ea69908e43a25
Shiva Ram Reddy <kathishivaram@gmail.com>
2020-06-13 23:43:02 +0530

    Initial Setup

---
+++ ExpenseCare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpenseCare
+//
+//  Created by Shiva Ram Reddy on 13/06/20.
+//  Copyright © 2020 Shiva Ram Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
